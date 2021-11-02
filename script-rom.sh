1532c34b6b79b4fa2f3c5765f29d5eb143997099
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2021-11-01 19:00:26 -0600

    I working on AEX ONLY

--- script-rom.sh
+++ script-rom.sh
@@ -9,35 +9,12 @@ export KBUILD_BUILD_HOST="EroticHost"
 export ALLOW_MISSING_DEPENDENCIES=true
 
 # CCACHE UMMM!!! Cooks my builds fast
-echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_DIR=/home/charlyffag/ccache
 ccache -M 25G
 
 # Build ROM
-echo -e "What rom you want build?"
-echo -e "1) AOSiP 2) AospExtended 3) ColtOS"
 . build/envsetup.sh
-read op
-if [ $op = 1 ]
-then
-  echo "You are building AOSiP!"
-  lunch aosip_surya-userdebug
-  make kronic -j8
-elif [ $op = 2 ]
-then
-  echo "You are building AospExtended!"
-  lunch aosp_surya-userdebug
-  make aex -j8
-elif [ $op = 3 ]
-then
-  echo "You are building ColtOS!"
-  lunch colt_surya-userdebug
-  make colt -j8
-else
-  echo "WTTTTTTTTF!"
-fi
-
-
-
+lunch aosp_surya-userdebug
+make aex -j8
