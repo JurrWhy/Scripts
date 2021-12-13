5d42e94a31574a1823a5ca527ea11dacb2defcd9
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2021-12-13 12:43:21 -0600

    scripts: Minnor update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -5,18 +5,17 @@
 
 # Export idk
 export KBUILD_BUILD_USER="Charly"
-export KBUILD_BUILD_HOST="EroticHost"
-export ALLOW_MISSING_DEPENDENCIES=true
+export KBUILD_BUILD_HOST="EroticHot"
 
 # CCACHE UMMM!!! Cooks my builds fast
 export CCACHE_EXEC="$(command -v ccache)"
 export CCACHE_SLOPPINESS=time_macros,include_file_mtime,file_macro
 export USE_CCACHE=true
-export CCACHE_DIR=/home/charlyffag/ccache
+export CCACHE_DIR=/root/ccache
 export CCACHE_CPP2=yes
 ccache -M 30G
 
 # Build ROM
 . build/envsetup.sh
 lunch lineage_surya-userdebug
-make bacon -j8
+make bacon -j16
