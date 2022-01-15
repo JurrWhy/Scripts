550b8f56847694c28b87166daa008ab513936bb5
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2022-01-15 13:13:19 -0600

    Update build-rom.sh

--- build-rom.sh
+++ build-rom.sh
@@ -3,10 +3,6 @@
 # Copyright (C) 2021 TheStrechh <carlosarriagacm@gmail.com>
 #
 
-# Export idk
-export KBUILD_BUILD_USER="Charly"
-export KBUILD_BUILD_HOST="EroticHot"
-
 # CCACHE UMMM!!! Cooks my builds fast
 export CCACHE_EXEC="$(command -v ccache)"
 export CCACHE_SLOPPINESS=time_macros,include_file_mtime,file_macro
