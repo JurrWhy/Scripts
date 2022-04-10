027288014b0ca5aaadf3dec732370baa545f3ffb
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2022-04-10 18:03:37 -0500

    CAF RIP now is CLO

--- merge-caf-tag.sh
+++ merge-caf-tag.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2022 TheStrechh (Carlos Arriaga)
 #
-# Basic scrip for merge CAF
+# Basic scrip for merge CLO
 #
 
 # Colors
@@ -15,34 +15,34 @@ echo "${grn} Initiating... ${end}"
 echo
 
 # Fetch kernel version from user
-read -p "<Enter your Kernel version:> " VERSION
+read -p "Enter your Kernel version: " VERSION
 echo
 
 sleep 1
 
-# Fetch CAF tag from user
-read -p "<Enter the CAF tag you want to merge:> " TAG
+# Fetch CLO tag from user
+read -p "Enter the CodeLinaro tag you want to merge: " TAG
 echo
 
 sleep 1
 
 # Merging
-echo "${blu} ||===Starting merge===|| ${end}"
+echo "${blu} ||=== Starting merge ===|| ${end}"
 echo
 
-    git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $TAG &&
+    git fetch https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0.git $TAG &&
 	git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
 
-    git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $TAG &&
+    git fetch https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api.git $TAG &&
 	git merge -X subtree=drivers/staging/fw-api FETCH_HEAD
 
-    git fetch https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $TAG &&
+    git fetch https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn.git $TAG &&
 	git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD
 
-    git fetch https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $TAG &&
+    git fetch https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel.git $TAG &&
 	git merge -X subtree=techpack/audio FETCH_HEAD
 
-    git fetch https://source.codeaurora.org/quic/la/kernel/msm-$VERSION $TAG &&
+    git fetch https://git.codelinaro.org/clo/la/kernel/msm-$VERSION.git $TAG &&
 	git merge FETCH_HEAD
 
 sleep 1
