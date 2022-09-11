124b7d98629ca7a9cad08a7fc2861394f484eac2
ᴄᴀʀʟᴏꜱ ᴀʀʀɪᴀɢᴀ <37526095+TheStrechh@users.noreply.github.com>
2022-09-11 10:56:58 -0500

    Update

--- build-rom.sh
+++ build-rom.sh
@@ -5,12 +5,13 @@
 
 # Replace "root" with your own SSH Username in lowercase
 username=root
+romname=lineage
 
 # CCACHE UMMM!!! Cooks my builds fast
 echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export CCACHE_DIR=/root/ccache/$username
+export CCACHE_DIR=/$username/ccache/$romname
 ccache -M 40G
 
 # Prepare Environment and Device
