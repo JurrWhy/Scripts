b0974c77d816430fe1243ff1e48f3331465e65f2
JurrWhy <jurrivhrnb@gmail.com>
2023-04-18 20:59:28 -0600

    Update

--- tree.sh
+++ tree.sh
@@ -1,7 +1,7 @@
 # Jurrwhy
 
 # Hola mundo
-git clone -b qpr1 https://github.com/Surya-Android-13/device_xiaomi_surya.git device/xiaomi/surya
+git clone -b extended https://github.com/Surya-Android-13/device_xiaomi_surya.git device/xiaomi/surya
 git clone -b thirteen https://github.com/Surya-Android-13/vendor_xiaomi_surya.git vendor/xiaomi/surya
 git clone -b lineage-20 https://github.com/Surya-Android-13/kernel_xiaomi_surya.git kernel_xiaomi_surya
 rm -rf packages/resources/devicesettings && git clone -b thirteen https://github.com/CC-Surya/packages_resources_devicesettings.git packages/resources/devicesettings
